52003662e627ff947cfe0bb4ae4939e3c0371e15
Imranidrees1234 <imranidrees053@gmail.com>
2024-04-27 02:51:31 +0500

    Initial commit

---
+++ src/components/Navbar/MobileNav/MobileNav.jsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+// import './MobileNav.css'
+
+// export const MobileNav = ({ isOpen, toggleMenu}) => {
+//   return (
+//     <>
+//         <div
+//             className={'mobile-menu ${isOpen ? "active" : ""}'}
+//             onClick={toggleMenu} 
+//             >
+
+//                 <div className="mobile-menu-container">
+//                     <img src="" alt="upload" />
+
+//                     <ul>
+//                         <li>
+//                             <a href="https://www.youtube.com/results?search_query=portfolio+react+project+with+source+code" className="menu-item">Home</a>
+//                         </li>
+//                         <li>
+//                             <a href="https://www.youtube.com/results?search_query=portfolio+react+project+with+source+code" className="menu-item">Skills</a>
+//                         </li>
+//                         <li>
+//                             <a href="https://www.youtube.com/results?search_query=portfolio+react+project+with+source+code" className="menu-item">Work Experience</a>
+//                         </li>
+//                         <li>
+//                             <a href="https://www.youtube.com/results?search_query=portfolio+react+project+with+source+code" className="menu-item">Contact Me</a>
+//                         </li>
+
+//                         <button className="contact-btn" onClick={() => {}}>
+//                         Hire Me
+//                         </button>
+
+//                     </ul>
+//                 </div>
+//             </div>
+//     </>
+//   )
+// }
+// export default MobileNav;
